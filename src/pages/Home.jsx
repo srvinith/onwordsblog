f5b8ced3c3efccffedd209f2102da09fb29237bd
Gokilavani-A <133311326+Gokilavani-A@users.noreply.github.com>
2023-08-05 10:26:18 +0530

    home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
-
+import '../Assets/css/home.css'
 const Home = () => {
   return (
-    <div>Home</div>
+    <>
+    <section className='blog-background image-fluid'>
+      <div className="contaier-fluid">
+        <div className="row">
+
+        </div>
+      </div>
+    </section>
+    </>
+
+
   )
 }
 
