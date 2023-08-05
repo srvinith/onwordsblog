18b22414aa526d6f7d45d431857836855a54d082
Gokilavani-A <133311326+Gokilavani-A@users.noreply.github.com>
2023-08-05 12:20:33 +0530

    {home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,13 +3,14 @@ import '../Assets/css/home.css'
 const Home = () => {
   return (
     <>
-    <section className='blog-background image-fluid'>
-      <div className="contaier-fluid">
-        <div className="row">
-
+      <section className='blog-background '>
+        <div className='onwords-home'>
+          <h1>ONWORDS BLOG</h1>
+          <h3>Discover stories, thinking, and expertise
+            <br></br>from writers on any topic.</h3>
+          <a href="#"><button className='blog-btn'>Get Started</button></a>
         </div>
-      </div>
-    </section>
+      </section>
     </>
 
 
