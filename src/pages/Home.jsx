436548abb2af5e5e07299dda5bf3c259e46c4641
Gokilavani-A <133311326+Gokilavani-A@users.noreply.github.com>
2023-08-05 13:22:07 +0530

    home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import '../Assets/css/home.css'
+// import '../Assets/images/robert-roy.png'
+import Health from '../Assets/images/health.png'
 const Home = () => {
   return (
     <>
@@ -9,6 +11,21 @@ const Home = () => {
           <h3>Discover stories, thinking, and expertise
             <br></br>from writers on any topic.</h3>
           <a href="#"><button className='blog-btn'>Get Started</button></a>
+        </div>
+      </section>
+      <section>
+        <div className="container">
+          <div className="row">
+            <div className="col-md-5">
+              <div>
+               </div>
+            </div>
+            <div className="col-md-3">
+              <img className='health' src={Health} alt="health" />
+            </div>
+            <div className="col-md-4"></div>
+          </div>
+          
         </div>
       </section>
     </>
