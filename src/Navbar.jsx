a6d0f5006ce14fa77f1e2a9bf1918b70f7b5f9fb
srvinith <r36041595@gmail.com>
2023-08-05 13:25:25 +0530

    blog saturday

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,77 +1,116 @@
-import React,{useState} from 'react'
+import React, { useState } from 'react'
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 
+
+// --------imgs
+import Logo from './Assets/images/logos.png'
 // -----icons
+
 import SearchIcon from '@mui/icons-material/Search';
+import MenuIcon from '@mui/icons-material/Menu';
+import PersonIcon from '@mui/icons-material/Person';
+import NotificationsIcon from '@mui/icons-material/Notifications';
+import EmailIcon from '@mui/icons-material/Email';
+import LockIcon from '@mui/icons-material/Lock';
+
+
 
 
 const Navbars = () => {
 
-    const [login, setLogin] = useState(false)
+  const [login, setLogin] = useState(false)
+  const[search,setSearch] = useState(false)
 
 
-    return (
-        <>
-      
-<div className="topNav">
-      <div className=""></div>
-      <div className="">
-        Logo
+  return (
+    <>
+
+      <div className="topNav bg-body-tertiary">
+        <div className=""><MenuIcon /></div>
+        <div className="">
+          <img src={Logo} alt="Logo" className='logo' />
+        </div>
+        <div className="searchbtn" onClick={()=>setSearch(true)}>
+          <SearchIcon /><span>Search</span>
+        </div>
       </div>
-      <div className="">
-        <SearchIcon />
-      </div>
-</div>
-      <Navbar collapseOnSelect expand="lg" className="bg-body-tertiary">
-      <Container fluid>
-        <Navbar.Brand href="#home">Onwords Blog</Navbar.Brand>
-        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-        <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
-            <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
-            </NavDropdown>
-          </Nav>
-          <Nav>
-            <Nav.Link href="#deets">More deets</Nav.Link>
-            <Nav.Link eventKey={2} href="#memes">
-              Dank memes
-            </Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-        <Navbar.Collapse className="justify-content-end">
-          <button onClick={()=>setLogin(!login)}>Login</button>
-          <Navbar.Text>
-            Signed in as: <a href="#login">Mark Otto</a>
-          </Navbar.Text>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
-    
- 
-          
-            {
-                login ?<div classNameNameName="login-box ">
-hgjf
-                </div> : null
-            }
-        </>
+      <Navbar collapseOnSelect expand="lg" className="nav-bot">
+        <Container>
+          <Navbar.Brand href="#home"></Navbar.Brand>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="me-auto mx-4">
+              <Nav.Link href="#features" className='mx-4'>Home</Nav.Link>
+              <Nav.Link href="#pricing" className='mx-4'>Blog</Nav.Link>
+              <NavDropdown title="Categories" id="collasible-nav-dropdown" className='mx-4'>
+                <NavDropdown.Item href="">Action</NavDropdown.Item>
+                <NavDropdown.Item href="">
+                  Another action
+                </NavDropdown.Item>
+                <NavDropdown.Item href="">Something</NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="">
+                  Separated link
+                </NavDropdown.Item>
+              </NavDropdown>
+            </Nav>
+
+          </Navbar.Collapse>
+          <Navbar.Collapse className="justify-content-end">
+            <div className="end-nav-li">
+              <button className='btn btn-primary'>CreateBlog</button>
+              <PersonIcon onClick={() => setLogin(!login)} className='icon-nav' />
+              <NotificationsIcon className='icon-nav' />
+            </div>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+
+      {
+        login ? <div className="login-box ">
+
+          <div className="signin">
+            <h2>Login</h2>
+            <div className="form-group">
+              <PersonIcon />
+              <input type="text" placeholder='Name' />
+            </div>
+            <div className="form-group">
+              <EmailIcon />
+              <input type="email" placeholder='Email' />
+            </div>
+            <div className="form-group">
+              <LockIcon />
+              <input type="password" placeholder='Password' />
+            </div>
+
+            <a href="">Forgot Password</a> <br />
+
+            <button className='btn btn-primary mt-3'>Signin</button>
+          </div>
+
+        </div> : null
+      }
+
+      {
+        search ? <div className='searchBg'>
+          <div className="searchcloser" >
+          <h2 className='closemark' onClick={()=>setSearch(false)}>X</h2>
+          <div className="search-inp">
+           <div className="inp">
+            
+           </div>
+          </div>
+          </div>
+        </div>:null
+      }
+    </>
 
 
-    )
+  )
 }
 
 export default Navbars
