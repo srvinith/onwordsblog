e5bbe25256a328ffac8a045a73b2b3f842ac1f48
srvinith <r36041595@gmail.com>
2023-09-12 09:27:10 +0530

    react add

--- src/pages/CreateBlog.jsx
+++ src/pages/CreateBlog.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import ReactQuill from "react-quill";
 import "react-quill/dist/quill.snow.css";
 import { Link } from 'react-router-dom'
+import axios from 'axios'
 
 const CreateBlog = () => {
 
@@ -18,43 +19,81 @@ const CreateBlog = () => {
             ["clean"],
         ],
     };
-    const [value, setValue] = useState("");
-    const [title, setTitle] = useState("")
-    console.log(value)
-   
+
+
+    // ============
+
+
+    const [title, setTitle] = useState('');
+    const [blogText, setBlogText] = useState('');
+    const [tags, setTags] = useState('');
+    const [category, setCategory] = useState('');
+    const [newCategory, setNewCategory] = useState('');
+    const [message, setMessage] = useState('');
+
+    const handleCreateBlog = async () => {
+        try {
+            const payload = {
+                title,
+                blog_text: blogText,
+                tags,
+                category: newCategory || category,
+                
+                user_id: 'YourUserIDHere', // Set the user ID accordingly
+            };
+
+            const response = await axios.post('http://192.168.1.8:8000/store_blog', payload);
+
+            if (response.status === 200) {
+                setMessage('Blog created successfully.');
+                // Optionally, you can redirect to another page or update the UI.
+            } else {
+                setMessage('An error occurred while creating the blog.');
+            }
+        } catch (error) {
+            setMessage('An error occurred while creating the blog.');
+        }
+
+    }
+
+
+    // ============
+
+
+
     return (
         <>
             <div className="container">
                 <form action="/">
-
+                    <p>{message && <p>{message}</p>}</p>
                     <div className="editor-container">
                         <div className="top-editor-btn">
                             <button className="btn btn-primary">Darft & Save</button>
-                            <Link to="/blogstore"> <button className="btn btn-primary">Publish</button></Link>
+                            <Link to="/blogstore"> <button className="btn btn-primary" onClick={handleCreateBlog}>Publish</button></Link>
                         </div>
                         <input type="text" required placeholder="Title" className="title-inp"
-                            onChange={event => setTitle(event.target.value)}
+                            onChange={setTitle}
                         />
-                        <p><input type="hidden" value={title} /></p>
-                        <ReactQuill modules={modules} theme="snow" onChange={setValue} placeholder="The content starts here..." className="text-editor" />
-                    </div> 
+                        <input type="text"  />
+                        <ReactQuill modules={modules} theme="snow" placeholder="The content starts here..." onChange={setBlogText} className="text-editor" />
+                    </div>
                 </form>
                
-                    <div className="bottom-create-inp">
-                        <form action="" className="tags-inp">
-                            <label for="tags">Tags (comma separated):</label>
-                            <input type="text" id="tags" name="tags" />
-                        </form>
-                        <select name="category" id="category">
-                            <option value="" disabled selected>Select a category or enter a new one</option>
-                            <option value="food" >Food</option>
-                            <option value="food" >Data Science</option>
-                        </select>
-                    </div>
-                
+                <div className="bottom-create-inp">
+                    <form action="" className="tags-inp">
+                        <label for="tags">Tags (comma separated):</label>
+                        <input type="text" id="tags" name="tags" onChange={setTags} />
+                    </form>
+                    <select name="category" id="category">
+                        <option value={setCategory} disabled selected>Select a category or enter a new one</option>
+                        <option value={setNewCategory} >{setNewCategory}</option>
+
+                    </select>
+                </div>
+
 
             </div>
-           
+
 
         </>
 
